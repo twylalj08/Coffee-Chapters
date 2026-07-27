49f1cf7ce1c146f4c089c0ca850c9c874084b41a
twylalj08 <twylalj08@gmail.com>
2026-07-27 15:18:38 -0600

    Update library.js

--- library.js
+++ library.js
@@ -1,3 +1,13 @@
 const starterLibrary = [
-
+  {
+    id: 1,
+    title: "Claimed by the Rugged Mountain Man",
+    author: "Rica Lane",
+    status: "Finished",
+    rating: 2,
+    spice: 3,
+    favoriteCharacter: "Dane",
+    wouldReadAgain: false,
+    notes: "Dane always treated Jennifer like an equal and bought Jennifer an art studio to pursue her dreams."
+  }
 ];
